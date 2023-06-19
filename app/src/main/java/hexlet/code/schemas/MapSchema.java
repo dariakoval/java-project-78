3bdbb6e7aa8acae801f32ee9d571a7ffed03512f
dariakoval <dasharibkavmore666@mail.ru>
2023-06-19 17:24:47 +0400

    update files

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -1,13 +1,9 @@
 package hexlet.code.schemas;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public final class MapSchema extends BaseSchema {
-    private Map<String, BaseSchema> valuesChecks;
-
     public MapSchema() {
-        this.valuesChecks = new HashMap<>();
         addCheck("required", value -> value instanceof Map || value == null);
     }
 
@@ -21,20 +17,11 @@ public final class MapSchema extends BaseSchema {
         return this;
     }
 
-    @SuppressWarnings("unchecked")
     public MapSchema shape(Map<String, BaseSchema> schemas) {
-        this.valuesChecks = schemas;
-        addCheck("shape", value -> checkMapValues((Map<String, Object>) value));
+        addCheck("shape", value -> schemas.entrySet().stream().allMatch(e -> {
+            Object v = ((Map) value).get(e.getKey());
+            return e.getValue().isValid(v);
+        }));
         return this;
-    }
-
-    public boolean checkMapValues(Map<String, Object> map) {
-        StringSchema stringSchema = (StringSchema) valuesChecks.get("name");
-        boolean isValidName = stringSchema.isValid(map.get("name"));
-
-        NumberSchema numberSchema = (NumberSchema) valuesChecks.get("age");
-        boolean isValidAge = numberSchema.isValid(map.get("age"));
-
-        return isValidName && isValidAge;
     }
 }
